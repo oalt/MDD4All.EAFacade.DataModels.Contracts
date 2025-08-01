056153a94c96bed3fe275e720b5230d32af09934
oalt <oa@lacheweg21.de>
2025-08-01 17:01:39 +0200

    Add repository object descriptor

---
+++ src/MDD4All.EAFacade.DataModels.Contracts/Extensions/RepositoryObjectDescriptor.cs
@@ -0,0 +1,13 @@
+﻿namespace MDD4All.EAFacade.DataModels.Contracts.Extensions
+{
+    public class RepositoryObjectDescriptor
+    {
+        public string ConnectionString { get; set; }
+
+        public string GUID { get; set; } = string.Empty;
+
+        public string Name { get; set; } = "<Nothing selected>";
+
+        public ObjectType ObjectType { get; set; } = ObjectType.otRepository;
+    }
+}
